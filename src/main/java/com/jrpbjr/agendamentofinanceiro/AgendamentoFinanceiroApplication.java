58562c447476a1bfa7de256b5ba1ac957bac2f3b
Jrpbjr <jrpbjr@gmail.com>
2023-11-08 21:27:50 -0300

    commit Inicial

---
+++ src/main/java/com/jrpbjr/agendamentofinanceiro/AgendamentoFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.jrpbjr.agendamentofinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrpbjr/agendamentofinanceiro/AgendamentoFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrpbjr.agendamentofinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
