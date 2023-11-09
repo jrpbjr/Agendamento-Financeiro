9be71c0d019b3c0e7daa89da1c84a5893325ed41
Jrpbjr <jrpbjr@gmail.com>
2023-11-09 19:53:13 -0300

    model para email e identificação do job

---
+++ src/main/java/com/jrpbjr/agendamentofinanceiro/payload/EmailRequest.java
@@ -0,0 +1,32 @@
+package com.jrpbjr.agendamentofinanceiro.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Setter
+@Getter
+public class EmailRequest {
+    @Email
+    @NotNull
+    private String email;
+
+    @NotEmpty
+    private String suject;
+
+    @NotEmpty
+    private String body;
+
+    @NotNull
+    private LocalDateTime dateTime;
+
+    @NotNull
+    private ZoneId timeZone;
+
+
+}
---
+++ src/main/java/com/jrpbjr/agendamentofinanceiro/payload/EmailResponse.java
@@ -0,0 +1,21 @@
+package com.jrpbjr.agendamentofinanceiro.payload;
+
+public class EmailResponse {
+
+    private boolean success;
+    private String jobId;
+    private String jobGroup;
+    private String message;
+
+    public EmailResponse(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+    public EmailResponse(boolean success, String jobId, String jobGroup, String message){
+        this.success = success;
+        this.message =  message;
+        this.jobId = jobId;
+        this.jobGroup = jobGroup;
+    }
+
+}
