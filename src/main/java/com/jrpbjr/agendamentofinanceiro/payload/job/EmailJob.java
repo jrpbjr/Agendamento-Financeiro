d31963c4674c467eea0dc60451408c7dbb44673e
Jrpbjr <jrpbjr@gmail.com>
2023-11-10 16:31:40 -0300

    implentação send email

--- src/main/java/com/jrpbjr/agendamentofinanceiro/payload/job/EmailJob.java
+++ src/main/java/com/jrpbjr/agendamentofinanceiro/payload/job/EmailJob.java
@@ -1,12 +1,56 @@
 package com.jrpbjr.agendamentofinanceiro.payload.job;
 
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
 
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
+
+@Component
 public class EmailJob extends QuartzJobBean {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private MailProperties mailProperties;
+
     @Override
     protected void executeInternal(JobExecutionContext jobExecutioncontext) throws JobExecutionException {
+
+        JobDataMap jobDataMap = jobExecutioncontext.getMergedJobDataMap();
+
+        String subject = jobDataMap.getString("subject");
+        String body = jobDataMap.getString("body");
+        String recipientEmail = jobDataMap.getString("email");
+
+        sendMail(mailProperties.getUsername(), recipientEmail, subject, body);
+    }
+
+    private void sendMail(String frontEmail, String toEmail, String subject, String body){
+
+        try{
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, StandardCharsets.UTF_8.toString());
+            messageHelper.setSubject(subject);
+            messageHelper.setText(body, true);
+            messageHelper.setTo(toEmail);
+
+            mailSender.send(message);
+        }catch (MessagingException ex){
+            System.out.println(ex);
+        }
 
     }
 }
