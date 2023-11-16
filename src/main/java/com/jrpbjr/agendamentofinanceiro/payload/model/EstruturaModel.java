103fc9be42e825f7abfeed5f6ff587594843fc52
Jrpbjr <jrpbjr@gmail.com>
2023-11-16 12:50:47 -0300

    estrutura de serializabel para model

---
+++ src/main/java/com/jrpbjr/agendamentofinanceiro/payload/model/EstruturaModel.java
@@ -0,0 +1,7 @@
+package com.jrpbjr.agendamentofinanceiro.payload.model;
+
+import java.io.Serializable;
+
+public class EstruturaModel implements Serializable {
+    private static final long serialVersionUID = 5390409768131104604L;
+}
