eed220289cb2e0089dcb1f2f1cf188a311e20621
jrpbjr <jrpbjr@gmail.com>
2023-11-24 17:23:53 -0300

    Teste taxa do Tipo C

--- src/test/java/com/jrpbjr/agendamentofinanceiro/AgendamentoFinanceiroApplicationTests.java
+++ src/test/java/com/jrpbjr/agendamentofinanceiro/AgendamentoFinanceiroApplicationTests.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.jrpbjr.agendamentofinanceiro.payload.service.OperacaoService;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 @SpringBootTest
 class AgendamentoFinanceiroApplicationTests {
@@ -48,6 +50,41 @@ class AgendamentoFinanceiroApplicationTests {
 
 		Assertions.assertEquals(new Double(agendamentoAte30Dias.getValorTransferencia() * 0.1), agendamentoAte30Dias.getTaxa());
 		Assertions.assertEquals(new Double(agendamentoDemais.getValorTransferencia() * 0.08), agendamentoDemais.getTaxa());
+
+	}
+
+
+	@Test
+	public void taxaTipoCTest() throws NegocioException {
+		OperacaoModel agendamentoMaior30Dias = this.criarAgendamento( this.adicionarDia( new Date() , 35) , Tipo.C, 100.00);
+		OperacaoModel agendamentoAte30Dias = this.criarAgendamento( this.adicionarDia( new Date() , 30) , Tipo.C, 100.00);
+		OperacaoModel agendamentoAte25Dias = this.criarAgendamento( this.adicionarDia( new Date() , 22) , Tipo.C, 100.00);
+		OperacaoModel agendamentoAte20Dias = this.criarAgendamento( this.adicionarDia( new Date() , 19) , Tipo.C, 100.00);
+		OperacaoModel agendamentoAte15Dias = this.criarAgendamento( this.adicionarDia( new Date() , 13) , Tipo.C, 100.00);
+		OperacaoModel agendamentoAte10Dias = this.criarAgendamento( this.adicionarDia( new Date() , 8) , Tipo.C, 100.00);
+		OperacaoModel agendamentoAte5Dias = this.criarAgendamento( this.adicionarDia( new Date() , 3) , Tipo.C, 100.00);
+
+		List<OperacaoModel> listaAgendamentos = new ArrayList<OperacaoModel>();
+		listaAgendamentos.add( agendamentoMaior30Dias );
+		listaAgendamentos.add( agendamentoAte30Dias );
+		listaAgendamentos.add( agendamentoAte25Dias );
+		listaAgendamentos.add( agendamentoAte20Dias );
+		listaAgendamentos.add( agendamentoAte15Dias );
+		listaAgendamentos.add( agendamentoAte10Dias );
+		listaAgendamentos.add( agendamentoAte5Dias );
+
+		for (OperacaoModel OperacaoModel : listaAgendamentos) {
+			OperacaoModel.setTaxa( this.operacaoService.calcularTaxa(OperacaoModel) );
+			this.operacaoService.validarTaxa(OperacaoModel);
+		}
+
+		Assertions.assertEquals(new Double(agendamentoMaior30Dias.getValorTransferencia() * 0.012), agendamentoMaior30Dias.getTaxa());
+		Assertions.assertEquals(new Double(agendamentoAte30Dias.getValorTransferencia() * 0.021), agendamentoAte30Dias.getTaxa());
+		Assertions.assertEquals(new Double(agendamentoAte25Dias.getValorTransferencia() * 0.043), agendamentoAte25Dias.getTaxa());
+		Assertions.assertEquals(new Double(agendamentoAte20Dias.getValorTransferencia() * 0.054), agendamentoAte20Dias.getTaxa());
+		Assertions.assertEquals(new Double(agendamentoAte15Dias.getValorTransferencia() * 0.067), agendamentoAte15Dias.getTaxa());
+		Assertions.assertEquals(new Double(agendamentoAte10Dias.getValorTransferencia() * 0.074), agendamentoAte10Dias.getTaxa());
+		Assertions.assertEquals(new Double(agendamentoAte5Dias.getValorTransferencia() * 0.083), agendamentoAte5Dias.getTaxa());
 
 	}
 
