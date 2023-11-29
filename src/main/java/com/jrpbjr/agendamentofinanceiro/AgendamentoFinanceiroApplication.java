def20b63a4bb1b9c8061719acc506e77a9b65653
jrpbjr <jrpbjr@gmail.com>
2023-11-29 16:56:11 -0300

    Configuração Basica Swagger.

--- src/main/java/com/jrpbjr/agendamentofinanceiro/AgendamentoFinanceiroApplication.java
+++ src/main/java/com/jrpbjr/agendamentofinanceiro/AgendamentoFinanceiroApplication.java
@@ -2,10 +2,14 @@ package com.jrpbjr.agendamentofinanceiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableCaching
+@EnableAsync
 public class AgendamentoFinanceiroApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/jrpbjr/agendamentofinanceiro/payload/config/SwaggerConfiguration.java
@@ -0,0 +1,20 @@
+package com.jrpbjr.agendamentofinanceiro.payload.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
